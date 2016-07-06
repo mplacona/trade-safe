89f78d4fa13241b54f963207a06029b0fe29714d
mplacona <marcos.placona@gmail.com>
2016-07-06 15:41:48 +0100

    Clean up unit tests

--- app/src/test/java/uk/co/placona/tradesafe/view/CreateActivityTest.java
+++ app/src/test/java/uk/co/placona/tradesafe/view/CreateActivityTest.java
@@ -3,7 +3,6 @@ package uk.co.placona.tradesafe.view;
 import android.widget.EditText;
 import android.widget.VideoView;
 
-import org.assertj.android.api.preference.EditTextPreferenceAssert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,12 +16,10 @@ import uk.co.placona.tradesafe.databinding.ActivityCreateBinding;
 
 import static org.assertj.android.api.Assertions.assertThat;
 
-//
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
 public class CreateActivityTest {
     private CreateActivity activity;
-    private ActivityCreateBinding binding;
     private EditText reference_txt;
     private EditText description_text;
     private VideoView video_view;
@@ -57,46 +54,4 @@ public class CreateActivityTest {
         assertThat(video_view).isNotNull();
         assertThat(video_view).isVisible();
     }
-
-//    @Test
-//    public void shouldHaveVisibleListView() throws Exception {
-//        assertThat(rvTrades).isNotNull();
-//        assertThat(rvTrades).isVisible();
-//    }
-//
-//    @Test
-//    public void shouldDisplayListViewWhenUserCancels() throws Exception {
-//        fab.performClick();
-//
-//        ShadowActivity.IntentForResult intentForResult = Shadows.shadowOf(activity).peekNextStartedActivityForResult();
-//        assertNotNull(intentForResult);
-//
-//        Shadows.shadowOf(activity).receiveResult(
-//                new Intent(MediaStore.ACTION_VIDEO_CAPTURE),
-//                Activity.RESULT_OK,
-//                null);
-//
-//
-//    }
-//
-//    @Test
-//    public void clickingAddVideoShouldStartCameraActivity() {
-//        fab.performClick();
-//
-//        Intent startedIntent = Shadows.shadowOf(activity).getNextStartedActivity();
-//        Intent expectedIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-//        assertThat(startedIntent).isEqualTo(expectedIntent);
-//    }
-//
-//    @Test
-//    public void buttonClickShouldStartNewActivity() throws Exception
-//    {
-//        fab.performClick();
-//
-//        ShadowActivity shadowActivity = Shadows.shadowOf(activity);
-//        Intent startedIntent = shadowActivity.getNextStartedActivity();
-//        ShadowIntent shadowIntent = Shadows.shadowOf(startedIntent);
-//        assertEquals(shadowIntent.getIntentClass().getCanonicalName(),CreateActivity.class.getName());
-//
-//    }
 }
